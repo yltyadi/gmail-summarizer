d124f9daed7d7f62d7f6aa4eb694fe03ebbeb58a
yltyadi <adi.yeltay@nu.edu.kz>
2022-06-22 14:36:28 +0600

    created chrome extension

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState} from 'react';
-import axios from "axios";
+// import axios from "axios";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -9,14 +9,14 @@ function App() {
   const handleTextToSummarize = (event) => {
     setInputValue(event.target.value);
   };
-
-  const formdata = new FormData();
-  formdata.append("key", "6b08e93b581ef423fd9001035f019564");
-  formdata.append("txt", inputValue);
-  formdata.append("sentences", "3");
   
   const handleSummarizeText = (event) => {
     event.preventDefault();
+
+    const formdata = new FormData();
+    formdata.append("key", "6b08e93b581ef423fd9001035f019564");
+    formdata.append("txt", inputValue);
+    formdata.append("sentences", "3");
     
     const requestOptions = {
         method: 'POST',
@@ -27,17 +27,26 @@ function App() {
     fetch("https://api.meaningcloud.com/summarization-1.0", requestOptions)
       .then(response => response.json())
       .then((data) => setResult(data.summary))
-      .catch(error => console.log('error', error));
+      .catch(error => setResult('error', error));
   };
+
+  // document.querySelector(".ii").innerText
 
   return (
     <div className="App">
       <h1>Email Summary</h1>
       <form className='input-form'>
-        <input value={inputValue} className="input-text" type='text' placeholder='put your text here' align='center' onChange={handleTextToSummarize}></input>
+        <textarea 
+          value={inputValue} 
+          className="input-text" 
+          type='text' 
+          placeholder='put your text here' 
+          data-gramm="false" 
+          onChange={handleTextToSummarize}>
+        </textarea>
         <button className="input-button" type='submit' onClick={handleSummarizeText}>Summarize</button>
       </form>
-      <h2 className='result'>{result}</h2>
+      <h3 className='result'>{result}</h3>
     </div>
   );
 }
