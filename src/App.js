6516f3c15d3de0179c58f5e29fd00632a942d3fa
yltyadi <adi.yeltay@nu.edu.kz>
2022-08-02 23:00:29 +0600

    working summarization directly inside gmail

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       function: function addBtn() {
         // checking if the button exists to prevent from creating another button 
         if (!document.getElementById('custom-btn')) {
-          let node = document.querySelector('.gb_qe.gb_oe'); // G-tF is inside the single letter header
+          let node = document.querySelector('.gb_qe.gb_oe');
           let btn = document.createElement('button');
           btn.setAttribute('id', 'custom-btn');
           btn.style.backgroundColor = '#80ed99';
@@ -37,27 +37,46 @@ function App() {
           node.insertBefore(btn, node.children[0]);
           btn.addEventListener('click', () => {
             // create result div once the button is clicked
-            let resultDiv;
-            if (!document.getElementById('custom-result')) {
-              let node = document.querySelector('.a3s.aiL ');
-              resultDiv = document.createElement('div')
-              resultDiv.setAttribute('id', 'custom-result');
-              resultDiv.style.width = '98%';
-              resultDiv.style.minHeight = '20px';
-              resultDiv.style.overflow = 'hidden';
-              resultDiv.style.backgroundColor = '#80ed99';
-              resultDiv.style.borderRadius = '15px';
-              resultDiv.style.textAlign = 'justify';
-              resultDiv.style.padding = '5px';
-              resultDiv.style.marginBottom = '5px';
-              resultDiv.style.color = '#021311';
-              resultDiv.style.fontWeight = 'bold';
-              node.insertBefore(resultDiv, node.children[0]);
+            if (document.getElementById('custom-result')) {
+              document.getElementById('custom-result').remove();
             }
-            chrome.storage.sync.get("result", ({ result }) => {
-              console.log(result);
-              resultDiv.innerHTML = result;
-            });
+            let node = document.querySelector('.a3s.aiL ');
+            let resultDiv = document.createElement('div');
+            resultDiv.setAttribute('id', 'custom-result');
+            resultDiv.style.width = '98%';
+            resultDiv.style.minHeight = '20px';
+            resultDiv.style.overflow = 'hidden';
+            resultDiv.style.backgroundColor = '#80ed99';
+            resultDiv.style.borderRadius = '15px';
+            resultDiv.style.textAlign = 'justify';
+            resultDiv.style.padding = '5px';
+            resultDiv.style.marginBottom = '5px';
+            resultDiv.style.color = '#021311';
+            resultDiv.style.fontWeight = '600';
+            node.insertBefore(resultDiv, node.children[0]);
+
+            const formdata = new FormData();
+            formdata.append("key", process.env.REACT_APP_API_KEY);
+            formdata.append("txt", window.getSelection().toString()); // passing selected text from background.js OR inputValue
+            formdata.append("sentences", "3");
+            
+            const requestOptions = {
+                method: 'POST',
+                body: formdata,
+                redirect: 'follow'
+            };
+
+            fetch("https://api.meaningcloud.com/summarization-1.0", requestOptions)
+              .then(response => response.json())
+              .then((data) => {
+                resultDiv.innerHTML = data.summary;
+              })
+              .catch(error => console.log(error));
+
+            // resultDiv.innerHTML = result;
+            // chrome.storage.sync.get("result", ({ result }) => {
+            //   resultDiv.innerHTML = result;
+            // });
           });
         }
       }
@@ -72,7 +91,7 @@ function App() {
     event.preventDefault();
 
     const formdata = new FormData();
-    formdata.append("key", "6b08e93b581ef423fd9001035f019564");
+    formdata.append("key", process.env.REACT_APP_API_KEY);
     formdata.append("txt", text); // passing selected text from background.js OR inputValue
     formdata.append("sentences", "3");
     
@@ -103,7 +122,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Email Summarizer</h1>
+      <h1>summarize.me</h1>
       {/* <form className='input-form'>
         <textarea 
           value={inputValue} 
